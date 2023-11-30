70ffcbb0cfebe1a091f62e3e28827e70b7b30d01
NikolozK34 <kekelidze97@gmail.com>
2023-11-30 14:59:03 +0400

    Add project files.

---
+++ davaleba1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace davaleba1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pirveli ricxvi: ");
+
+            double number1 = Convert.ToDouble(Console.ReadLine());
+           
+
+            Console.WriteLine("Meore ricxvi: ");
+
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = number1 + number2;
+
+            Console.WriteLine("Rixvebis jami aris: " + sum);
+
+        }
+    }
+}
